e493b20fa275f7e65b2f8071e7c04d935d1ab2d3
19681018 <73780087+19681018@users.noreply.github.com>
2021-11-18 13:42:09 +0900

    import JS file

---
+++ 중간고사/유승호_jsonTest.js
@@ -0,0 +1,8 @@
+const data = { 
+    "Data":"Code","__EMPTY":42,
+    "__EMPTY":"id","__EMPTY_1":"name",
+    "Data":"items","__EMPTY":1,"__EMPTY_1":"foo",
+    "__EMPTY":2,"__EMPTY_1":"bar"
+}; 
+var n = 3; 
+console.log('아이템 아이디는 ' + id_number + '이고, 아이템 이름은 ' + name + '입니다')
